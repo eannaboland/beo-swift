9e36b577d48ede816fd7b5550c88a546b28ce183
eannaboland <eannaboland@gmail.com>
2017-03-13 17:18:45 +0000

    Fixed previous commit.

--- Beo/AppDelegate.swift
+++ Beo/AppDelegate.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Eanna Boland on 20/02/2017.
 //  Copyright © 2017 Eanna Boland. All rights reserved.
-//  Credit to Tommy Mitchell, taught me everything I know.
 //
 
 import UIKit
